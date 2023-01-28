a1a100a9c024f6cc4f1bd42bf7b4bb095ccb8d97
Michael McClimon <michael@mcclimon.org>
2023-01-27 22:35:06 -0500

    gi: switch to non-deprecated syscall interface
    Helpful to read the package docs there, bud.

--- cmd/gi/main.go
+++ cmd/gi/main.go
@@ -4,7 +4,8 @@ import (
 	"os"
 	"os/exec"
 	"strings"
-	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 // This program exists so that when I type `gi tpull` it does the right thing.
@@ -17,7 +18,7 @@ func main() {
 	os.Args[0] = git
 	os.Args[1] = strings.TrimPrefix(os.Args[1], "t")
 
-	if err := syscall.Exec(git, os.Args, os.Environ()); err != nil {
+	if err := unix.Exec(git, os.Args, os.Environ()); err != nil {
 		panic(err)
 	}
 }
