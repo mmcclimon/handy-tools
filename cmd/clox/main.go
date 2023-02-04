a9c96f0fa2825f64019112061f842c1e332712f9
Michael McClimon <michael@mcclimon.org>
2023-02-04 16:37:29 -0500

    clox: add support for specifying a time
    So you can say `clox 11:45` and see what time it was at some not- now
    time.

--- cmd/clox/main.go
+++ cmd/clox/main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
-	now := time.Now()
-	format := "Monday, 15:04"
-
 	us, _ := time.LoadLocation("America/New_York")
 	au, _ := time.LoadLocation("Australia/Melbourne")
 
-	fmt.Printf("🇺🇸 : %s <-- you are here\n", now.In(us).Format(format))
-	fmt.Printf("🌐 : %s\n", now.In(time.UTC).Format(format))
-	fmt.Printf("🇦🇺 : %s\n", now.In(au).Format(format))
+	now := time.Now()
+
+	if len(os.Args) > 1 {
+		today := now.Format("2006-01-02")
+		when, err := time.ParseInLocation("2006-01-02 15:04", today+" "+os.Args[1], us)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error parsing time, using now (%s)\n", err)
+		} else {
+			now = when
+		}
+	}
+
+	format := "Monday, 15:04"
+
+	fmt.Printf("%s: %s <-- you are here\n", "🇺🇸", now.In(us).Format(format))
+	fmt.Printf("%s: %s\n", "🌐", now.In(time.UTC).Format(format))
+	fmt.Printf("%s: %s\n", "🇦🇺", now.In(au).Format(format))
 }
